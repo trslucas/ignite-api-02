b8d1400d5297b4ca54e7aa01ccd5e34271d77f22
Lucasm <trslucas@outlook.com>
2023-07-11 00:13:26 -0300

    Validating environment variables

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,11 @@
 import 'dotenv/config'
 import { knex as setupNext, Knex } from 'knex'
+import { env } from './env'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE NOT FOUND')
-}
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
